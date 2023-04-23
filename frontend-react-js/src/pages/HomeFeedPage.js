e98700ee854f36b8d49ad24a9aa36d4a979edacc
ctmazarura95 <61659172+ctmazarura95@users.noreply.github.com>
2023-04-23 19:54:11 +0000

    cognito

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -6,55 +6,66 @@ import DesktopSidebar     from '../components/DesktopSidebar';
 import ActivityFeed from '../components/ActivityFeed';
 import ActivityForm from '../components/ActivityForm';
 import ReplyForm from '../components/ReplyForm';
+import { Auth } from 'aws-amplify';
 
 // [TODO] Authenication
-import Cookies from 'js-cookie'
+
 
 export default function HomeFeedPage() {
   const [activities, setActivities] = React.useState([]);
   const [popped, setPopped] = React.useState(false);
   const [poppedReply, setPoppedReply] = React.useState(false);
   const [replyActivity, setReplyActivity] = React.useState({});
-  const [user, setUser] = React.useState(null);
+  
   const dataFetchedRef = React.useRef(false);
 
-  const loadData = async () => {
-    try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
-      const res = await fetch(backend_url, {
-        method: "GET"
-      });
-      let resJson = await res.json();
-      if (res.status === 200) {
-        setActivities(resJson)
-      } else {
-        console.log(res)
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  
 
-  const checkAuth = async () => {
-    console.log('checkAuth')
-    // [TODO] Authenication
-    if (Cookies.get('user.logged_in')) {
+// set a state
+const [user, setUser] = React.useState(null);
+
+const loadData = async () => {
+  try {
+    const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
+    const res = await fetch(backend_url, {
+      method: "GET"
+    });
+    let resJson = await res.json();
+    if (res.status === 200) {
+      setActivities(resJson)
+    } else {
+      console.log(res)
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// check if we are authenicated
+const checkAuth = async () => {
+  Auth.currentAuthenticatedUser({
+    // Optional, By default is false. 
+    // If set to true, this call will send a 
+    // request to Cognito to get the latest user data
+    bypassCache: false 
+  })
+  .then((user) => {
+    console.log('user',user);
+    return Auth.currentAuthenticatedUser()
+  }).then((cognito_user) => {
       setUser({
-        display_name: Cookies.get('user.name'),
-        handle: Cookies.get('user.username')
+        display_name: cognito_user.attributes.name,
+        handle: cognito_user.attributes.preferred_username
       })
-    }
-  };
+  })
+  .catch((err) => console.log(err));
+};
 
-  React.useEffect(()=>{
-    //prevents double call
-    if (dataFetchedRef.current) return;
-    dataFetchedRef.current = true;
-
-    loadData();
-    checkAuth();
-  }, [])
-
+// check when the page loads if we are authenicated
+React.useEffect(()=>{
+  loadData();
+  checkAuth();
+}, [])
   return (
     <article>
       <DesktopNavigation user={user} active={'home'} setPopped={setPopped} />
